61e74ad1138d6005f71428461236a0c350c449e0
bensonoyugi <bensonoyugi@protonmail.com>
2024-10-30 03:42:04 +0300

    Set up hero section with h1 and tagline paragraph

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-	return <>Home</>;
+	return (
+		<>
+			<section className="w-full bg-primary min-h-[530px] flex justify-center items-center flex-col py-10 px-6 pattern">
+				<h1 className="heading">
+					Pitch Your Startup, <br /> Connect With Entrepreneurs
+				</h1>
+
+				<p className="font-medium text-[20px] text-white max-w-2xl text-center break-words !max-w-3xl">
+					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+				</p>
+			</section>
+		</>
+	);
 }
