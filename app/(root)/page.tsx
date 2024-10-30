f8b8e341fc1288994c0e1673809c1708a677ae4f
bensonoyugi <bensonoyugi@protonmail.com>
2024-10-30 11:48:29 +0300

    Complete SearchForm in home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+	searchParams
+}: {
+	searchParams: Promise<{ query: string }>;
+}) {
+	const query = (await searchParams).query;
+
 	return (
 		<>
 			<section className="w-full bg-primary min-h-[530px] flex justify-center items-center flex-col py-10 px-6 pattern">
@@ -9,6 +17,8 @@ export default function Home() {
 				<p className="font-medium text-[20px] text-white max-w-2xl text-center break-words !max-w-3xl">
 					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
 				</p>
+
+				<SearchForm query={query} />
 			</section>
 		</>
 	);
